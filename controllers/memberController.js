a87bcb0e8bbdfe28be33adc557af4c6348237958
vaibhav agarwal <your-email@example.com>
2025-10-09 14:53:17 +0530

    fixed  code 09-10-2025 2:54pm

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -3,6 +3,7 @@ const Member = require("../model/Member");
 exports.getAllMembers = async (req, res) => {
   try {
     const members = await Member.find();
+    // const members = await Member.find();
     res.status(200).json(members);
   } catch (error) {
     console.error(error);
